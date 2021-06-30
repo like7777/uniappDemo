65467e0ba8d77937cfd206936d2b1625e48cfc8d
like7777 <1748031151@qq.com>
2021-06-30 09:29:57 +0000

    6.30

---
+++ ShopDemo/pages/Community/Community.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<scroll-view  :scroll-top="scrollTop" :scroll-left="scrollLeft" scroll-y="true" class="scroll-Y" >
+				<view  class="scroll-view-item " @click="options(1)" :class="isActive == 1 ? 'active' : ''">家居生活</view>
+				<view  class="scroll-view-item " @click="options(2)" :class="isActive == 2 ? 'active' : ''">摄影设计</view>
+				<view  class="scroll-view-item " @click="options(3)" :class="isActive == 3 ? 'active' : ''">明星美女</view>
+				<view  class="scroll-view-item " @click="options(4)" :class="isActive == 4 ? 'active' : ''">空间设计</view>
+				<view  class="scroll-view-item " @click="options(5)" :class="isActive == 5 ? 'active' : ''">户型装饰</view>
+				<view  class="scroll-view-item " @click="options(6)" :class="isActive == 6 ? 'active' : ''">广告摄影</view>
+				<view  class="scroll-view-item " @click="options(7)" :class="isActive == 7 ? 'active': ''">摄影学习</view>
+				<view  class="scroll-view-item " @click="options(8)" :class="isActive == 8 ? 'active' : ''">摄影器材</view>
+				<view  class="scroll-view-item " @click="options(9)" :class="isActive == 9 ? 'active' : ''">明星写真</view>
+				<view  class="scroll-view-item " @click="options(10)" :class="isActive == 10 ? 'active' : ''">清纯甜美</view>
+				<view  class="scroll-view-item " @click="options(11)" :class="isActive == 11 ? 'active' : ''">一二三四</view>
+				<view  class="scroll-view-item " @click="options(12)" :class="isActive == 12 ? 'active' : ''">我不知道</view>
+		</scroll-view>
+		<view class="">
+			1234
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isActive:1,
+				scrollTop:100,
+				scrollLeft:100
+			}
+		},
+		methods: {
+			options(e){
+				this.isActive = e
+			}
+		}
+	}
+</script>
+
+<style>
+	.scroll-Y {
+		width: 100px;
+		display: inline-block;
+	}
+	
+	.scroll-view-item {
+		height: 60px;
+		line-height: 60px;
+		display: block;
+		width: 100px;
+		background: #fff;
+		border-bottom: 1px solid #ccc;
+		text-align: center;
+	}
+	
+	.active {
+		color: #fff;
+		background: #b50e03;
+	}
+</style>
--- ShopDemo/pages/index/index.vue
+++ ShopDemo/pages/index/index.vue
@@ -1,6 +1,47 @@
 <template>
 	<view class="content">
-		<text>首页</text>
+		<swiper class="index-swiper" :indicator-dots="false" :autoplay="true" :interval="3000" :duration="1000" >
+			<swiper-item v-for="(item,index) in swipers" :key="index">
+				<image :src="item"></image>
+			</swiper-item>
+		</swiper>
+		<view class="tips">
+			<view class="tipsBlock">
+				<view class="rounded">
+					<image src="../../static/icon/超市.png" mode=""></image>
+				</view>
+				<view class="text">
+					<text>超市</text>
+				</view>
+			</view>
+			<view class="tipsBlock">
+				<view class="rounded">
+					<image src="../../static/icon/联系我们.png" mode=""></image>
+				</view>
+				<view class="text">
+					<text>联系</text>
+				</view>
+			</view>
+			<navigator class="tipsBlock" url="../Community/Community">
+				<view class="rounded">
+					<image src="../../static/icon/社区图片.png" mode=""></image>
+				</view>
+				<view class="text">
+					<text>图片</text>
+				</view>
+			</navigator>
+			<view class="tipsBlock">
+				<view class="rounded">
+					<image src="../../static/icon/视频.png" mode=""></image>
+				</view>
+				<view class="text">
+					<text>视频</text>
+				</view>
+			</view>
+		</view>
+		<view class="recommended">
+			<text>推荐商品</text>
+		</view>
 	</view>
 </template>
 
@@ -26,39 +67,68 @@
 							console.log(111)
 						}
 						this.swipers = res.data
-						console.log(this.swipers)
+						console.log(this.swipers[0])
 					}
 				})
-				
-			}
+			},
 		}
 	}
 </script>
 
 <style>
-	.content {
+	.index-swiper{
+		width: 100%;
+		height: 468upx;
+	}
+		
+	image{
+		width: 100%;
+	}
+	
+	.tips {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+		align-content: stretch;
+		justify-content: space-evenly;
+		align-items: baseline;
+	}
+	
+	.tips image {
+		width: 30px;
+		height: 30px;
+	}
+	
+	.rounded {
+		border-radius: 50%;
+		background: pink;
+		width: 50px;
+		height: 50px;
+		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
+	
+	.tipsBlock {
 		display: flex;
-		justify-content: center;
+		align-items: center;
+		flex-direction: column
 	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	
+	.recommended {
+		border: 2px  solid #ccc;
+		border-left: none;
+		border-right: none;
+		margin-top: 10px;
+		height: 50px;
+		line-height: 50px;
+		color: red;
+	}
+	
+	.recommended text {
+		width: 60%;
+		margin: 0 auto;
+		display: block;
+		text-align: center;
+		letter-spacing: 20px;
 	}
 </style>
